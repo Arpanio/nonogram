dfb4e09e6ce3aaacdcf2557b5d0b21cde26fb2d9
Arpan Das <arpan.das@publicissapient.com>
2020-12-29 13:37:32 +0000

    Added code to process multi-group constraints. Since contiguity detection is a subset of this, the contiguity detection is now deprecated (although not yet removed from code)

--- backend.py
+++ backend.py
@@ -1,10 +1,19 @@
 import re
 import constraint as cp
+import json
 from typing import List, Tuple
+from functools import partial
 
 
-def detect_groups():
-    pass
+def detect_groups(group_sums, *args):
+    str_ = "".join([str(a) for a in list(args)])
+    groups = re.split('0+', str_)
+    groups = [g for g in groups if g != '']
+    result_sums = [len(g) for g in groups]
+    if group_sums != result_sums:
+        return False
+    else:
+        return True
 
 
 def contiguity(*args):
@@ -15,7 +24,10 @@ def contiguity(*args):
         return True
 
 
-def backend(r: int, c: int, r_num: List[List[int]], c_num: List[List[int]], crossed_cells: List[Tuple[int, int]] = []):
+def backend(r: int, c: int,
+            r_num: List[List[int]],
+            c_num: List[List[int]],
+            crossed_cells: List[Tuple[int, int]] = None):
     # Setup CP problem object
     problem = cp.Problem()
     
@@ -24,7 +36,9 @@ def backend(r: int, c: int, r_num: List[List[int]], c_num: List[List[int]], cros
     problem.addVariables(variables, [0, 1])
     
     # Create starting constraints (crossed-out cells)
-    # ToDo
+    if crossed_cells and len(crossed_cells) != 0:
+        crossed_variables = [f"A_{row}_{col}" for row, col in crossed_cells]
+        problem.addConstraint(cp.InSetConstraint({0}), crossed_variables)
     
     # Create row-sum and column-sum constraints
     for row in range(r):
@@ -33,11 +47,16 @@ def backend(r: int, c: int, r_num: List[List[int]], c_num: List[List[int]], cros
         
         # Add constraint to CP object
         # Doesn't work because of https://github.com/python-constraint/python-constraint/issues/48
-        # Understand what's happening
+        # Understand what's happening ToDo
         # problem.addConstraint(lambda *args: sum(args) == row_sum, constraint_vars)
         
         problem.addConstraint(cp.ExactSumConstraint(row_sum), constraint_vars)
-        problem.addConstraint(contiguity, constraint_vars)
+        # problem.addConstraint(contiguity, constraint_vars)
+        
+        # multi-group constraints
+        group_details = r_num[row]
+        constraint_group = partial(detect_groups, group_details)
+        problem.addConstraint(constraint_group, constraint_vars)
         
     for col in range(c):
         constraint_vars = [v for v in variables if re.search(f"_{col}$", v)]
@@ -46,10 +65,37 @@ def backend(r: int, c: int, r_num: List[List[int]], c_num: List[List[int]], cros
         # Add constraint to CP object
         # problem.addConstraint(lambda *args: sum(args) == col_sum, constraint_vars)
         problem.addConstraint(cp.ExactSumConstraint(col_sum), constraint_vars)
-        problem.addConstraint(contiguity, constraint_vars)
+        # problem.addConstraint(contiguity, constraint_vars)
+
+        # multi-group constraints
+        group_details = c_num[col]
+        constraint_group = partial(detect_groups, group_details)
+        problem.addConstraint(constraint_group, constraint_vars)
         
     # Create multi-group constraints
     # ToDo
+    # for i, r_n in enumerate(r_num):
+    #     if len(r_n) == 1:
+    #         continue
+    #
+    #     constraint_vars = [v for v in variables if re.search(f"_{i}_", v)]
+    #     constraint_value = r_n
+    #     constraint_group = partial(detect_groups, constraint_value)
+    #     problem.addConstraint(constraint_group, constraint_vars)
     
     # Solve
-    return problem.getSolutions()
+    # Convert dict solution into array ToDO
+    solution = problem.getSolutions()
+    return solution
+
+
+if __name__ == '__main__':
+    print(json.dumps(backend(2, 2,
+                             [[1], [2]],
+                             [[2], [1]]),
+                     indent=2, sort_keys=True))
+    print(json.dumps(backend(2, 3,
+                             [[1], [2]],
+                             [[1], [1], [1]],
+                             [(0, 1)]),
+                     indent=2, sort_keys=True))
