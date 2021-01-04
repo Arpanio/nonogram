73a41b445ee7342ffde8effe2830b9929e6333d9
Arpan Das <arpan.das@publicissapient.com>
2021-01-04 10:29:00 +0000

    Made edits to convert output into array. Breaks tests due to change in output format.

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
 import re
 import constraint as cp
+import numpy as np
 import json
 from typing import List, Tuple
 from functools import partial
@@ -74,25 +75,46 @@ def backend(r: int, c: int,
     
     # Solve
     # Convert dict solution into array ToDO
-    solution = problem.getSolutions()
-    return solution
+    solutions = problem.getSolutions()
+    solutions_array = []
+    for solution in solutions:
+        variables_in_order = [solution[k] for k in sorted(solution)]
+        solutions_array.append(np.array(variables_in_order).reshape((r, c)))
+    
+    return solutions_array
 
 
 if __name__ == '__main__':
-    print(json.dumps(backend(2, 2,
-                             [[1], [2]],
-                             [[2], [1]]),
-                     indent=2, sort_keys=True))
-    print(json.dumps(backend(2, 3,
-                             [[1], [2]],
-                             [[1], [1], [1]],
-                             [(0, 1)]),
-                     indent=2, sort_keys=True))
-    print(json.dumps(backend(10, 10,
-                             [[4], [4], [2], [9], [10], [10], [9], [2], [5], [5]],
-                             [[2], [4], [4], [4, 2], [4, 2], [2, 4, 2], [2, 7], [10], [7], [4]]),
-                     indent=2, sort_keys=True))
-    print(json.dumps(backend(10, 10,
-                             [[5], [3, 1], [3, 1], [1, 2, 1], [10], [9], [3], [3], [3], [3]],
-                             [[2], [2], [2], [2], [2], [10], [10], [3, 6], [1, 2], [6]]),
-                     indent=2, sort_keys=True))
+    # print(json.dumps(backend(2, 2,
+    #                          [[1], [2]],
+    #                          [[2], [1]]),
+    #                  indent=2, sort_keys=True))
+    # print(json.dumps(backend(2, 3,
+    #                          [[1], [2]],
+    #                          [[1], [1], [1]],
+    #                          [(0, 1)]),
+    #                  indent=2, sort_keys=True))
+    # print(json.dumps(backend(10, 10,
+    #                          [[4], [4], [2], [9], [10], [10], [9], [2], [5], [5]],
+    #                          [[2], [4], [4], [4, 2], [4, 2], [2, 4, 2], [2, 7], [10], [7], [4]]),
+    #                  indent=2, sort_keys=True))
+    # print(json.dumps(backend(10, 10,
+    #                          [[5], [3, 1], [3, 1], [1, 2, 1], [10], [9], [3], [3], [3], [3]],
+    #                          [[2], [2], [2], [2], [2], [10], [10], [3, 6], [1, 2], [6]]),
+    #                  indent=2, sort_keys=True))
+    print(backend(2, 2,
+                  [[1], [2]],
+                  [[2], [1]]))
+    print(backend(2, 3,
+                  [[1], [2]],
+                  [[1], [1], [1]],
+                  [(0, 1)]))
+    print(backend(10, 10,
+                  [[4], [4], [2], [9], [10], [10], [9], [2], [5], [5]],
+                  [[2], [4], [4], [4, 2], [4, 2], [2, 4, 2], [2, 7], [10], [7], [4]]))
+    print(backend(10, 10,
+                  [[5], [3, 1], [3, 1], [1, 2, 1], [10], [9], [3], [3], [3], [3]],
+                  [[2], [2], [2], [2], [2], [10], [10], [3, 6], [1, 2], [6]]))
+    print(backend(10, 10,
+                  [[5], [2], [3], [9], [9], [1], [3], [2], [5], [2]],
+                  [[2], [3], [2], [9], [9], [9], [2], [4], [2], [3]]))
